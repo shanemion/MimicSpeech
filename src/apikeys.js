6b0e7a5452a71d6e97e316b7a219f871527f9ae7
shanemion <110140524+shanemion@users.noreply.github.com>
2023-07-26 19:01:08 -0700

    removed api keys

--- src/apikeys.js
+++ src/apikeys.js
@@ -1,4 +1,1 @@
-export const SPEECH_KEY = "69902698d44e4ed4b71d3d089909e8fc"
-export const SPEECH_REGION= "westus"
-
-export const OPENAI_KEY = "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"
+"hey"
